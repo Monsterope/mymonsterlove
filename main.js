f86a31e42226d4595c066bfb20f4ee075f01bc3d
unknown <dachawat.fang57@sskru.ac.th>
2019-04-28 00:15:27 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        firstname: "Dachawat",
+        lastname: "Fangnok",
+        nickname: "ope",
+    },
+    methods: {
+        getName: () => {
+            return "Monsterlove"
+        }
+    },
+})
